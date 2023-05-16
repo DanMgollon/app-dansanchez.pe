686a9c0a773b2f675d72688bee65173def46c13e
Kevin04c <kevincespedes2004@gmail.com>
2023-05-15 20:08:15 -0500

    Add react-hot-toast

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Auth } from '@/hooks'
 import { SessionProvider } from 'next-auth/react'
+import { Toaster } from 'react-hot-toast'
 
 export default function App ({ Component, pageProps: { session, ...pageProps } }: AppProps): JSX.Element {
   return (
     <SessionProvider>
       <Auth>
         <Component {...pageProps} />
+        <Toaster />
       </Auth>
     </SessionProvider>
   )
