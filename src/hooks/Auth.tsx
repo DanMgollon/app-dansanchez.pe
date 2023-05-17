cb16c9c863f8560506e64d5b18c53687b5def660
Kevin04c <kevincespedes2004@gmail.com>
2023-05-16 21:55:36 -0500

    Add Auth

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -1,5 +1,5 @@
 import type { User } from '@/interfaces'
-import { useAuthStore } from '@/store'
+import { useAreasStore, useAuthStore } from '@/store'
 import { useSession } from 'next-auth/react'
 import { useEffect } from 'react'
 import type { ReactNode, FC } from 'react'
@@ -11,12 +11,17 @@ interface Props {
 export const Auth: FC<Props> = ({ children }) => {
   const loginUser = useAuthStore(state => state.loginUser)
   const { data, status } = useSession()
+  const loadAreas = useAreasStore(state => state.loadAreas)
 
   useEffect(() => {
     if (status === 'authenticated') {
       loginUser(data.user as User)
     }
   }, [data, status])
+
+  useEffect(() => {
+    loadAreas()
+  }, [])
 
   return (
     <>
