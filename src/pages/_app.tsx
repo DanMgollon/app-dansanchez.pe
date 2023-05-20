6ce1354e5b74f312a78887a9641433fb49ab3efd
Kevin04c <kevincespedes2004@gmail.com>
2023-05-19 23:45:22 -0500

    Add spinner

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import '@/styles/Spinner.css'
 import type { AppProps } from 'next/app'
 import { Auth } from '@/hooks'
 import { SessionProvider } from 'next-auth/react'
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,1 +1,2 @@
 export * from './ToastDelete'
+export * from './Spinner'
