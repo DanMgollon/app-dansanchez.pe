70062ad67613defe8f7f34745743207c56f86011
Kevin04c <kevincespedes2004@gmail.com>
2023-05-22 18:03:03 -0500

    Add icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document (): JSX.Element {
   return (
     <Html lang="es">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/icon.ico" type="image/x-icon" />
+      </Head>
       <body>
         <Main />
         <NextScript />
