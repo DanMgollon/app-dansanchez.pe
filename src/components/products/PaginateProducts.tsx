65bd74ad71642a9b2166838538c991e5320c2ad0
Kevin04c <kevincespedes2004@gmail.com>
2023-05-22 22:35:02 -0500

    Update style product paginate

--- src/components/products/PaginateProducts.tsx
+++ src/components/products/PaginateProducts.tsx
@@ -18,12 +18,12 @@ export const PaginateProducts: FC = () => {
       previousLabel="<"
       nextLabel=">"
       pageLinkClassName="w-ful h-full"
-      previousClassName="bg-neutral-800 rounded border border-gray-300 px-4 py-2 text-sm font-medium text-white hover:bg-neutral-700 cursor-pointer"
-      nextClassName="bg-neutral-800 rounded border border-gray-300 px-4 py-2 text-sm font-medium text-white hover:bg-neutral-700 cursor-pointer"
+      previousClassName="bg-blue-700 rounded border border-gray-300 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700 cursor-pointer"
+      nextClassName="bg-blue-700 rounded border border-gray-300 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700 cursor-pointer"
       containerClassName="flex gap-2 items-center"
       pageClassName="bg-white rounded border border-gray-300 text-sm font-medium text-gray-700 hover:bg-gray-100 cursor-pointer px-4 py-2"
-      activeClassName='border-blue-500'
-      activeLinkClassName='text-blue-500'
+      activeClassName='!border-blue-400 !bg-blue-100 !hover:bg-blue-400'
+      activeLinkClassName='text-blue-800'
       onPageChange={handlePageClick}
     />
   )
