7df4d34d22ed5b37151592f7cc8cf3905665e444
Kevin04c <kevincespedes2004@gmail.com>
2023-06-14 14:16:53 -0500

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,13 @@
 import { getToken } from 'next-auth/jwt'
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 
 export async function middleware (req: NextRequest): Promise<NextResponse> {
-  const session = await getToken({ req })
-
-  if (session === null) {
-    return NextResponse.redirect(new URL('/', req.url))
+  if (req.nextUrl.pathname.startsWith('/dashboard')) {
+    const session = await getToken({ req })
+    if (session === null) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
   }
-  return NextResponse.next()
-}
 
-export const config = {
-  matcher: ['/dashboard/:path*']
+  return NextResponse.next()
 }
