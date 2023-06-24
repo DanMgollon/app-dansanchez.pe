e304f22dfcd3127a0b91203890f492b22a57aeb1
Kevin04c <kevincespedes2004@gmail.com>
2023-06-23 21:04:25 -0500

    Update middleware for protected route

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,5 +9,17 @@ export async function middleware (req: NextRequest): Promise<NextResponse> {
     }
   }
 
+  if (
+    req.nextUrl.pathname.startsWith('/api') &&
+      !req.nextUrl.pathname.startsWith('/api/auth')
+  ) {
+    const session = await getToken({ req })
+    if (session === null) {
+      return NextResponse.json({
+        message: 'Usted no tiene permisos para realizar esta acción'
+      }, { status: 401 })
+    }
+  }
+
   return NextResponse.next()
 }
