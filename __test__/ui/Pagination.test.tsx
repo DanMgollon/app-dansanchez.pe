98fdcb73c910739019ec2723c983f8f50b4cc2b8
Kevin04c <kevincespedes2004@gmail.com>
2023-07-10 02:14:32 -0500

    Add test Pagination

--- __test__/ui/Pagination.test.tsx
+++ __test__/ui/Pagination.test.tsx
@@ -16,11 +16,20 @@ describe('Test in Pagination.tsx', () => {
 
     const liElements = screen.getAllByRole('listitem')
 
-    expect(screen.getByText('<')).toBeInTheDocument()
-    expect(screen.getByText('>')).toBeInTheDocument()
     expect(liElements.length).toBe(pageCount - 1)
     expect(liElements[0].className).toBe(
       'bg-blue-700 rounded border border-gray-300 px-4 py-2 text-sm font-medium text-white hover:bg-blue-700 cursor-pointer'
     )
+  })
+
+  test('La clase debe cambiar cuando cambie de pagina', () => {
+    render(<Pagination {...props}/>)
+    const firstPage = screen.getByText('1')
+    const secondPage = screen.getByText('2')
+
+    fireEvent.click(secondPage)
+
+    expect(firstPage.parentElement?.className).toBe('bg-white rounded border border-gray-300 text-sm font-medium text-gray-700 hover:bg-gray-100 cursor-pointer px-4 py-2')
+    expect(secondPage.parentElement?.className).toBe('bg-white rounded border border-gray-300 text-sm font-medium text-gray-700 hover:bg-gray-100 cursor-pointer px-4 py-2 !border-blue-400 !bg-blue-100 !hover:bg-blue-400')
   })
 })
